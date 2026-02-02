2d3683936620bb6344818206c0f08e5c4279f4e0
Jeg <joelescanoguzman@gmail.com>
2026-02-02 19:45:04 -0400

    Crea el modelo "ViajesEspaciales"

---
+++ joel_escano_AP1_P1/Models/ViajesEspaciales.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace joel_escano_AP1_P1.Models
+{
+    public class ViajesEspaciales
+    {
+        [Key]
+        public int ViajeId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public DateTime Fecha { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Descripcion { get; set; }
+
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int Cantidad { get; set; }
+
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public float Costo { get; set; }
+    }
+}
